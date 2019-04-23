8f2c582a9f8c2e3b6a53f3ae5c64c15da6cbc3c0
Wolfgang <wolf@okwolf.com>
2019-04-22 22:09:39 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Hyperload
+
+[![npm](https://img.shields.io/npm/v/hyperload.svg)](https://www.npmjs.org/package/hyperload)
+
+A higher-order app and component for dynamic import of [Hyperapp v2](https://github.com/jorgebucaran/hyperapp) components. Requires support (or polyfill) for [dynamic imports](https://caniuse.com/#feat=es6-module-dynamic-import).
+
+## Installation
+
+### Node.js
+
+Install with npm / Yarn.
+
+<pre>
+npm i <a href=https://www.npmjs.com/package/hyperload>hyperload</a>
+</pre>
+
+Then with a module bundler like [rollup](https://github.com/rollup/rollup) or [webpack](https://github.com/webpack/webpack), use as you would anything else.
+
+```js
+import { withHyperload, Hyperload } from "hyperload";
+```
+
+Or using require.
+
+```js
+const { withHyperload, Hyperload } = require("hyperload");
+```
+
+### Browser
+
+Download the minified library from the [CDN](https://unpkg.com/hyperload).
+
+```html
+<script src="https://unpkg.com/hyperload"></script>
+```
+
+You can find the library in `window.hyperload`.
+
+## Example
+
+```js
+import { app, h } from "hyperapp";
+import { withHyperload, Hyperload } from "hyperload";
+
+const Loading = ({ error, otherProp }) => (
+  <span>{error ? `Error! ${error}` : `loading ${otherProps}...`}</span>
+);
+
+const LoadMyComponent = Hyperload({
+  // These are the required props
+  module: "./my-component",
+  loading: Loading,
+
+  otherProps: "will be passed to the loading and resolved components"
+});
+
+withHyperload(app)({
+  view: () => <LoadMyComponent />,
+  container: document.body
+});
+```
+
+Both the `loading` component and the dynamically imported module will receive the props that you pass to the `Hyperload` component. Errors encountered while importing your module will be passed to your `loading` component as the `error` prop.
+
+## License
+
+Hyperload is MIT licensed. See [LICENSE](LICENSE.md).
---
+++ src/index.js
@@ -0,0 +1,101 @@
+function assign(source, assignments) {
+  var result = {},
+    i;
+  for (i in source) result[i] = source[i];
+  for (i in assignments) result[i] = assignments[i];
+  return result;
+}
+
+export function Hyperload(props) {
+  return {
+    hyperload: true,
+    props: props
+  };
+}
+
+function makeUpdateLoadState(modulePath, state, dispatch) {
+  return function updateLoadState(props) {
+    var loadStateProp = {};
+    loadStateProp[modulePath] = assign(state.hyperload[modulePath], props);
+    dispatch(
+      assign(state, {
+        hyperload: assign(state.hyperload, loadStateProp)
+      })
+    );
+  };
+}
+
+function patchVdom(vdom, state, moduleCache, dispatch) {
+  if (typeof vdom === "object" && vdom !== null) {
+    for (var i in vdom.children) {
+      var child = vdom.children[i];
+      if (child.hyperload) {
+        var modulePath = child.props.module;
+        var loadState = state.hyperload[modulePath];
+        var cachedModule = moduleCache[modulePath];
+        var updateLoadState = makeUpdateLoadState(modulePath, state, dispatch);
+        if (!loadState) {
+          loadState = { loading: true };
+          updateLoadState(loadState);
+          import(modulePath)
+            .then(function(loaded) {
+              moduleCache[modulePath] = loaded.default || loaded;
+              updateLoadState({ loading: false, loaded: true });
+            })
+            .catch(function(error) {
+              updateLoadState({ loading: false, error: error });
+            });
+        }
+        if (loadState.loaded && cachedModule) {
+          vdom.children[i] = cachedModule(child.props);
+        } else if (loadState.error) {
+          vdom.children[i] = child.props.loading(
+            assign(child.props, {
+              error: loadState.error
+            })
+          );
+        } else if (loadState.loading) {
+          vdom.children[i] = child.props.loading(child.props);
+        }
+      } else {
+        patchVdom(child, state, moduleCache, dispatch);
+      }
+    }
+  }
+}
+
+export function withHyperload(nextApp) {
+  return function(props) {
+    var moduleCache = {};
+    var dispatch;
+    function enhancedInit(initialState) {
+      return [
+        initialState,
+        [
+          function(_, initDispatch) {
+            dispatch = initDispatch;
+            dispatch(props.init);
+            dispatch(function(state) {
+              return assign(state, {
+                hyperload: {}
+              });
+            });
+          }
+        ]
+      ];
+    }
+
+    function enhancedView(state) {
+      var vdom = props.view(state);
+      patchVdom(vdom, state, moduleCache, dispatch);
+      return vdom;
+    }
+
+    return nextApp(
+      assign(props, {
+        init: enhancedInit,
+        view: enhancedView
+      })
+    );
+  };
+}
